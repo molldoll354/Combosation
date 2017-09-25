bb4ef34dd885092f9b63ba615cedb1de5de0a34d
Michael Dellapi <mad758@nyu.edu>
2017-09-24 20:24:24 -0400

    Added in scripts for stanza 2-5

---
+++ Assets/Scripts/StanzaFourCouplet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StanzaFourCouplet : MonoBehaviour {
+
+	bool PickedRight;
+	bool PickedWrong;
+	string FourthStanza = "So long as men can breathe, or eyes can see,";
+	string LastLine4 = "So long lives this, and this gives life to thee.";
+	public float TimeToSwitch;
+	// Use this for initialization
+	void Start () {
+		PickedRight = false;
+		PickedWrong = false;
+		//TimeToSwitch = 50;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("" + TimeToSwitch);
+		if(Input.GetButton("Submit")){
+			ReadPoetry.activeString =FourthStanza;
+		}
+		if (COMBOLIST.RomanceCombo3.Check ()) {
+			PickedRight = true;
+			//Debug.Log ("Hit");
+		}
+		if (COMBOLIST.SincereCombo2.Check ()) {
+			PickedWrong = true;
+		}
+		if (COMBOLIST.JokingCombo1.Check ()) {
+			PickedWrong = true;
+		}
+
+		if (PickedRight == true) {
+			TimeToSwitch--;
+			ReadCorrect.correctString = LastLine4;
+			// Debug.Log ("" + PickedRight);
+
+		}
+		if (TimeToSwitch == 0) {
+			StanzaManager.Stanza1Active = true;
+		}
+	}
+}
---
+++ Assets/Scripts/StanzaTwo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StanzaTwo : MonoBehaviour {
+
+	bool PickedRight;
+	bool PickedWrong;
+	string SecondStanza = "Sometime too hot the eye of heaven shines,\nAnd often is his gold complexion dimmed;\nAnd every fair from fair sometime declines,";
+	string LastLine2 = "By chance, or nature’s changing course, untrimmed;";
+	public float TimeToSwitch;
+	// Use this for initialization
+	void Start () {
+		PickedRight = false;
+		PickedWrong = false;
+		//TimeToSwitch = 50;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("" + TimeToSwitch);
+		if(Input.GetButton("Submit")){
+			ReadPoetry.activeString = SecondStanza;
+		}
+		if (COMBOLIST.RomanceCombo2.Check ()) {
+			PickedRight = true;
+			//Debug.Log ("Hit");
+		}
+		if (COMBOLIST.SincereCombo2.Check ()) {
+			PickedWrong = true;
+		}
+		if (COMBOLIST.JokingCombo2.Check ()) {
+			PickedWrong = true;
+		}
+
+		if (PickedRight == true) {
+			TimeToSwitch--;
+			ReadCorrect.correctString = LastLine2;
+			// Debug.Log ("" + PickedRight);
+
+		}
+		if (TimeToSwitch == 0) {
+			StanzaManager.Stanza3Active = true;
+		}
+	}
+}
+
