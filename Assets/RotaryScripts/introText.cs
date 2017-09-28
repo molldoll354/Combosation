d4633341e4f92e9b7bc542238a02bec5f59e9b53
Michael Dellapi <mad758@nyu.edu>
2017-09-28 16:47:08 -0400

    I havent done anything substantial except make scripts

---
+++ Assets/RotaryScripts/introText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class introText : MonoBehaviour {
+
+	public static string activeString;
+	public Text intro;
+	public float timeToType;
+	public static float textPercentage = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("active string = " + activeString);
+		intro.text = "" + activeString;
+		int numberOfLettersToShow = (int)(activeString.Length * textPercentage);
+		intro.text = activeString.Substring (0, numberOfLettersToShow);
+		textPercentage += Time.deltaTime / timeToType;
+		textPercentage = Mathf.Min (1.0f, textPercentage);
+	}
+}
