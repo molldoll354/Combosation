d31dd3db46fb8235f3f3747577b709a4755905ed
Unknown <ls3805@nyu.edu>
2017-10-05 15:30:07 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Loren/Rotary/healthBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthBar : MonoBehaviour {
+	public GameObject player;
+	public Slider sliderVar;
+	// Use this for initialization
+	void Start () {
+		//sliderVar = GetComponent<healthBar> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sliderVar.value = player.GetComponent<playerHealth> ().health;
+	}
+}
---
+++ Assets/Loren/Rotary/playerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class playerHealth : MonoBehaviour {
+	public int health=5;
+	public Slider slidderVar;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Alpha1)){
+			health-=5;
+			Debug.Log ("my health is" + health);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha2)){
+			health+=5;
+			Debug.Log ("my health is" + health);
+		}
+		slidderVar.value = health;
+	}
+}
