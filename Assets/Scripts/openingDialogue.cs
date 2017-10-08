cdce017ee92cb28a912dc6485596b31ad1f160ab
Michael Dellapi <mad758@nyu.edu>
2017-10-08 18:07:50 -0400

    Dialogue scene and script added

--- Assets/Scripts/openingDialogue.cs
+++ Assets/Scripts/openingDialogue.cs
@@ -11,15 +11,32 @@ public class openingDialogue : MonoBehaviour {
 	public TextAsset textFile;
 	public string[] textLines;
 	public int currentLine;
+	public int lastLine;
+
+	public GameObject button;
 	// Use this for initialization
 	void Start () {
 		if (textFile != null) {
 			textLines = (textFile.text.Split ('\n'));
 		}
+		button.SetActive(false);
 	}
 	// Update is called once per frame
 	void Update () {
 			theText.text = textLines [currentLine];
+
+		string s = textLines [currentLine];
+		if (s.Contains ("Saki")) {//check if string contains a word*
+			theText.color = Color.black;
+		} else {
+			theText.color = Color.white;
+		}
+		if (s.Contains ("INPUT COMBO")) {
+			theText.text = "";
+			button.SetActive(true);
+		} else {
+			button.SetActive(false);
+		}
 
 			if (Input.GetKeyDown (KeyCode.RightArrow)) {
 				currentLine += 1;
@@ -28,9 +45,9 @@ public class openingDialogue : MonoBehaviour {
 			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
 				currentLine -= 1;
 			}
-				
-
-
+		if(Input.GetKeyDown(KeyCode.RightArrow)&& currentLine==lastLine){	
+			currentLine=0;
+		}
 		}
 	}
 
