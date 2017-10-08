455f9cb19c148ae9214970c814bdbd0de49ce1a3
Molly <molly354@comcast.net>
2017-10-08 18:09:04 -0400

    Round change works, needs feedback for player + AV Stuff

--- Assets/Scripts/ButtonSelection.cs
+++ Assets/Scripts/ButtonSelection.cs
@@ -9,24 +9,15 @@ public class ButtonSelection : MonoBehaviour
 	//max number of buttons needed to be pressed for this instance
 	public int ButtonsPressed;
 	//number of buttons that have been pressed
-	public float Timer = 11f;
+	public float Timer;
+	public float TimeToChange;
 	//time players have to press buttons
+	public int RoundNumber;
 
 	public Text timeText;
 	//the timer text display
 	public Text meterText;
-
-	//public float friend;
-	//friend meter, max of 10
-	//public float sad;
-	///sad meter, max of 10
-	//public float love;
-	//love meter, max of 10
-	//public float DisplayFriend; //the ints the player see
-	//public float DisplaySad;
-	//public float DisplayLove;
-
-	bool StopButtons; //determines whether the player can input stuff 
+	public bool StopButtons; //determines whether the player can input stuff
 
 	public float sadUp, loveUp, friendUp, sadChange, friendChange, loveChange;
 	public Slider sadnessBar,lovelyBar,friendlyBar;//sliders for the bars
@@ -34,13 +25,19 @@ public class ButtonSelection : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		MaxButtons = 10;
+		MaxButtons = 3;
 		StopButtons = false;
+		RoundNumber = 1;
+		TimeToChange = 5f;
+		Timer = 11f;
+//		RoundNumber = 1;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		Debug.Log ("" + TimeToChange);
+
 		sadnessBar.value = sadChange;
 		lovelyBar.value = loveChange;
 		friendlyBar.value = friendChange;
@@ -52,6 +49,30 @@ public class ButtonSelection : MonoBehaviour
 		friendUp = Mathf.Clamp(friendUp, 0f, 100f);
 		friendChange = Mathf.Clamp(friendChange, 0f, 100f);
 
+		if (RoundNumber == 1) {
+			MaxButtons = 3;
+			//Timer = 16f;
+		}
+		if (RoundNumber == 2) {
+			MaxButtons = 3;
+			//Timer = 16f;
+		}
+		if (RoundNumber == 3) {
+			MaxButtons = 3;
+			//Timer = 16f;
+		}
+		if (RoundNumber == 4) {
+			MaxButtons = 4;
+			//Timer = 11f;
+		} 
+		if (RoundNumber == 5) {
+			MaxButtons = 4;
+			//Timer = 11f;
+		}
+		if (RoundNumber == 6) {
+			MaxButtons = 5;
+			//Timer = 6f;
+		}
 
 		Debug.Log ("" + StopButtons);
 		//meterText.text = "Sad: " + DisplaySad + "\nFriend: " + DisplayFriend + "\nLove: " + DisplayLove;
@@ -75,7 +96,7 @@ public class ButtonSelection : MonoBehaviour
 			ButtonsPressed++;
 			//love += 2;
 			//sad--;
-			loveUp += 5;
+			loveUp += 10;
 			sadUp += 5;
 		}
 
@@ -85,6 +106,8 @@ public class ButtonSelection : MonoBehaviour
 			//sad--;
 			//friendliness+=friendUp;
 			//sadness -= friendDown;
+			friendUp += 10;
+			sadUp -= 5;
 		}
 
 		if (Input.GetKeyDown (KeyCode.D) && StopButtons == false) { //Wink. Ups sad and love by 1
@@ -93,29 +116,44 @@ public class ButtonSelection : MonoBehaviour
 			//love++;
 			//sadness+=sadUp;
 			//loveliness += loveUp;
-			print ("im actually working");
+			loveUp += 5;
+			sadUp += 5;
+
 		}
 
 		if (ButtonsPressed >= MaxButtons) {
-			Debug.Log ("You Did It!");
 			Timer = 0;
 			StopButtons = true;
 			friendChange = friendUp;
 			loveChange = loveUp;
 			sadChange = sadUp;
-			//DisplayFriend = friend;
-			//DisplaySad = sad;
-			//DisplayLove = love;
-
-			//timeText.
+			TimeToChange -= Time.deltaTime;
 		}
 		if (Timer < 0 && ButtonsPressed < MaxButtons) {
-			Debug.Log ("You dang fucked up son");
-			timeText.text = "You Lose";
-			//Timer = 0;
+			timeText.text = "Time Over";
+			//TimeToChange -= Time.deltaTime;
+
 		}
-		//}
+		if (TimeToChange < 0) {
+			ButtonsPressed = 0;
+			RoundNumber++;
+			TimeToChange = 5f;
+			Timer = 11f;
+			StopButtons = false;
+		}
 	}
+//	void RoundChange(){
+//		if (RoundNumber < 4) {
+//			ButtonSelection.MaxButtons = 3;
+//			ButtonSelection.Timer = 16f;
+//		} else if (RoundNumber < 6) {
+//			ButtonSelection.MaxButtons = 4;
+//			ButtonSelection.Timer = 11f;
+//		} if (RoundNumber == 6) {
+//			ButtonSelection.MaxButtons = 5;
+//			ButtonSelection.Timer = 6f;
+//		}
+//	}
 	//void FixedUpdate (){
 		
 	//}
--- Assets/Scripts/EventChanger.cs
+++ Assets/Scripts/EventChanger.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class EventChanger : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+//	public static int RoundNumber;
+//	// Use this for initialization
+//	void Start () {
+//		RoundNumber = 1;
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//
+//			
+//	}
+//	void RoundChange(){
+//		if (RoundNumber < 4) {
+//			ButtonSelection.MaxButtons = 3;
+//			ButtonSelection.Timer = 16f;
+//		} else if (RoundNumber < 6) {
+//			ButtonSelection.MaxButtons = 4;
+//			ButtonSelection.Timer = 11f;
+//		} if (RoundNumber == 6) {
+//			ButtonSelection.MaxButtons = 5;
+//			ButtonSelection.Timer = 6f;
+//		} if(RoundNumber >6){
+//			//End Game
+//		}
+//	}
 }
