6452759eefb13e5ec658579c8e0d66a272212b20
Michael Dellapi <mad758@nyu.edu>
2017-10-09 23:20:18 -0400

    The dialogue script has been changed and is ready for testing

--- Assets/Scripts/openingDialogue.cs
+++ Assets/Scripts/openingDialogue.cs
@@ -14,6 +14,8 @@ public class openingDialogue : MonoBehaviour {
 	public int lastLine;
 
 	public GameObject button;
+
+	public ButtonSelection selectionScript;
 	// Use this for initialization
 	void Start () {
 		if (textFile != null) {
@@ -24,7 +26,7 @@ public class openingDialogue : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 			theText.text = textLines [currentLine];
-
+		lastLine = textLines.Length;
 		string s = textLines [currentLine];
 		if (s.Contains ("Saki")) {//check if string contains a word*
 			theText.color = Color.black;
@@ -36,6 +38,16 @@ public class openingDialogue : MonoBehaviour {
 			button.SetActive(true);
 		} else {
 			button.SetActive(false);
+		}
+
+		if((selectionScript.sadnessBar.value>50)&&(selectionScript.sadnessBar.value>selectionScript.friendlyBar.value)){
+			theText.text="I'm really sad.";
+		}
+		if((selectionScript.lovelyBar.value>50)&&(selectionScript.lovelyBar.value>selectionScript.friendlyBar.value)){
+			theText.text="Maybe the right fighter was right in front of me all along...";
+		}
+		if((selectionScript.friendlyBar.value>50)&&(selectionScript.friendlyBar.value>selectionScript.lovelyBar.value)){
+			theText.text="Ya know, you are always there for me!";
 		}
 
 			if (Input.GetKeyDown (KeyCode.RightArrow)) {
