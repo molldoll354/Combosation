a8c01e0247848a866968ce8f4b10fe3a386f59fc
Loren <ls3805@nyu.edu>
2017-10-17 13:49:09 -0400

    Judgement SCENE!!!!
    made an end scene that keeps track of the values of the bars cross scene.

---
+++ Assets/Loren/scripts/gameMaster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameMaster : MonoBehaviour {
+	public static float sadTotalGM, friendTotalGM, loveTotalGM;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.I)){
+			Debug.Log ("here is sad: " + sadTotalGM + " and here is the friendTotal: " + friendTotalGM + " and here is the loveTotal: " + loveTotalGM + ".");
+		}
+	}
+}
---
+++ Assets/Loren/scripts/openingDialogue1.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class openingDialogue1 : MonoBehaviour {
+	public GameObject textBox;
+	public Text theText;
+
+
+
+	public TextAsset textFile;
+	public string[] textLines;
+	public int currentLine;
+	public int lastLine;
+
+	public GameObject button;
+
+	public ButtonSelection selectionScript;
+	// Use this for initialization
+	void Start () {
+		if (textFile != null) {
+			textLines = (textFile.text.Split ('\n'));
+		}
+		button.SetActive(false);
+	}
+	// Update is called once per frame
+	void Update () {
+			theText.text = textLines [currentLine];
+		lastLine = textLines.Length;
+		string s = textLines [currentLine];
+		if (s.Contains ("Saki")) {//check if string contains a word*
+			theText.color = Color.black;
+		} else {
+			theText.color = Color.white;
+		}
+		if (s.Contains ("C O M B O S A T I O N")) {
+			Application.LoadLevel ("Loren/scenes/healthbarscene 1");
+			button.SetActive(true);
+		} 
+
+//		if((currentLine>10)&&(selectionScript.sadnessBar.value>50)&&(selectionScript.sadnessBar.value>selectionScript.friendlyBar.value)){
+//			theText.text="I'm really sad.";
+//		}
+//		if((currentLine>10)&&(selectionScript.lovelyBar.value>50)&&(selectionScript.lovelyBar.value>selectionScript.friendlyBar.value)){
+//			theText.text="Maybe the right fighter was right in front of me all along...";
+//		}
+//		if((currentLine>10)&&(selectionScript.friendlyBar.value>50)&&(selectionScript.friendlyBar.value>selectionScript.lovelyBar.value)){
+//			theText.text="Ya know, you are always there for me!";
+//		}
+
+			if (Input.GetKeyDown (KeyCode.RightArrow)) {
+				currentLine += 1;
+			}
+
+			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+				currentLine -= 1;
+			}
+		if(Input.GetKeyDown(KeyCode.RightArrow)&& currentLine==lastLine){	
+			currentLine=0;
+		}
+		}
+	}
+
