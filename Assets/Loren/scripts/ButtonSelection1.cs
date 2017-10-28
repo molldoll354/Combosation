726b12ba18332651145e8e81eb104cc3760d20dc
Loren <ls3805@nyu.edu>
2017-10-28 18:32:19 -0400

    REworking Input
    REworking how player's input is taken and sorted. Currently code checks if the combo exists in a list or is just a random one.

--- Assets/Loren/scripts/ButtonSelection1.cs
+++ Assets/Loren/scripts/ButtonSelection1.cs
@@ -13,23 +13,38 @@ public class ButtonSelection1 : MonoBehaviour
 	public float TimeToChange;
 	//time players have to press buttons
 	public int RoundNumber;
-	public int maxRound;//max number of round we want.
+
 	public Text timeText;
 	//the timer text display
 	public Text meterText;
 	public bool StopButtons; //determines whether the player can input stuff
 
 	public GameObject roundButton;
-	public float sadUp, loveUp, friendUp, //rate of Change for the bars
-					sadChange, friendChange, loveChange;//current value for each bar's change.
-	public static float	sadTotal, friendTotal, loveTotal;//total of the bar
+	public float sadUp, loveUp, friendUp, sadChange, friendChange, loveChange;
 	public Slider sadnessBar,lovelyBar,friendlyBar;//sliders for the bars
 
-
+	public AudioSource audioButton;
+	public AudioSource roundEndAudio;
+	public AudioClip buttonClick;
+	public float buttonPitch = 0;
+	public float baseButtonPitch;
 
 	public GameObject happyFace, sadFace, loveFace;
 	public GameObject flatter, joke, wink, chat, neutral;
-	bool clearButtons=false;
+
+	public Sprite blankSprite; //sprites and array to control the button icons
+	public Sprite flatterSprite;
+	public Sprite winkSprite;
+	public Sprite chatSprite;
+	public Sprite jokeSprite;
+	public GameObject [] buttons;	
+
+	bool resetIcons = false; //sets the button icons back to blank
+
+	bool clearButtons=false;//clears previous player inputs
+
+	bool playedEndAudio = false; //this is some caveman shit to make the round end audio work
+
 
 
 	// Use this for initialization
@@ -40,7 +55,9 @@ public class ButtonSelection1 : MonoBehaviour
 		RoundNumber = 1;
 		TimeToChange = 2f;
 		Timer = 11f;
-//		RoundNumber = 1;
+		baseButtonPitch = audioButton.pitch;
+
+		//		RoundNumber = 1;
 
 
 		happyFace.SetActive (false);
@@ -50,15 +67,21 @@ public class ButtonSelection1 : MonoBehaviour
 		roundButton.SetActive (false);
 
 		flatter.SetActive (false);
-			joke.SetActive (false);
+		joke.SetActive (false);
 		wink.SetActive (false);
 		chat.SetActive (false);
 		neutral.SetActive (true);
+
+		buttons [3].GetComponent<SpriteRenderer> ().enabled = false;
+		buttons [4].GetComponent<SpriteRenderer> ().enabled = false;
+
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
 	{
+
+
 		Debug.Log ("" + TimeToChange);
 
 		sadnessBar.value = sadChange;
@@ -97,13 +120,10 @@ public class ButtonSelection1 : MonoBehaviour
 
 		sadUp = Mathf.Clamp(sadUp, 0f, 100f);
 		sadChange = Mathf.Clamp(sadChange, 0f, 100f);
-		sadTotal=Mathf.Clamp(sadTotal,0f,100f);
 		loveUp = Mathf.Clamp(loveUp, 0f, 100f);
 		loveChange = Mathf.Clamp(loveChange, 0f, 100f);
-		loveTotal=Mathf.Clamp(loveTotal,0f,100f);
 		friendUp = Mathf.Clamp(friendUp, 0f, 100f);
 		friendChange = Mathf.Clamp(friendChange, 0f, 100f);
-		friendTotal=Mathf.Clamp(friendTotal,0f,100f);
 
 		if (RoundNumber == 1) {
 			MaxButtons = 3;
@@ -119,6 +139,7 @@ public class ButtonSelection1 : MonoBehaviour
 		}
 		if (RoundNumber == 4) {
 			MaxButtons = 4;
+			buttons [3].GetComponent<SpriteRenderer> ().enabled = true;
 			//Timer = 11f;
 		} 
 		if (RoundNumber == 5) {
@@ -127,19 +148,29 @@ public class ButtonSelection1 : MonoBehaviour
 		}
 		if (RoundNumber == 6) {
 			MaxButtons = 5;
+			buttons [4].GetComponent<SpriteRenderer> ().enabled = true;
+
 			//Timer = 6f;
 		}
+
 
 		Debug.Log ("" + StopButtons);
 		//meterText.text = "Sad: " + DisplaySad + "\nFriend: " + DisplayFriend + "\nLove: " + DisplayLove;
 		//Mathf.Round (Timer);
 		timeText.text = "Time: " + Mathf.Floor (Timer); //rounds the timer display so thta you dont see shitty decimals
 		if (Timer >= 0) {
-			
+
 			Timer -= Time.deltaTime;
 		}
+
+
 		//if (StopButtons == false) {
 		if (Input.GetKeyDown (KeyCode.W) && StopButtons == false) { //Chat. ups friend by 1, lowers love by 1, lowers sad by 1
+			audioButton.pitch = audioButton.pitch + buttonPitch;
+			audioButton.clip = buttonClick;
+			audioButton.Play();
+			buttonPitch+=0.075f;
+			buttons [ButtonsPressed].GetComponent<SpriteRenderer> ().sprite = chatSprite;
 			ButtonsPressed++;
 			//friend++;
 			//love--;
@@ -153,6 +184,11 @@ public class ButtonSelection1 : MonoBehaviour
 			sadUp -= 5;
 		}
 		if (Input.GetKeyDown (KeyCode.A) && StopButtons == false) { //Compliment. Ups love meter by 2, lowers sad by 1
+			audioButton.pitch = audioButton.pitch + buttonPitch;
+			audioButton.clip = buttonClick;
+			audioButton.Play();
+			buttonPitch+=0.075f;
+			buttons [ButtonsPressed].GetComponent<SpriteRenderer> ().sprite = flatterSprite;
 			ButtonsPressed++;
 			//love += 2;
 			//sad--;
@@ -165,11 +201,16 @@ public class ButtonSelection1 : MonoBehaviour
 			if (sadnessBar.value >= 50) {
 				sadUp += 5;
 			}
-				
-			}
+
+		}
 
 
 		if (Input.GetKeyDown (KeyCode.S) && StopButtons == false) { //Joke. Ups friend meter 2, lowers sad by 1
+			audioButton.pitch = audioButton.pitch + buttonPitch;
+			audioButton.clip = buttonClick;
+			audioButton.Play();
+			buttonPitch+=0.075f;
+			buttons [ButtonsPressed].GetComponent<SpriteRenderer> ().sprite = jokeSprite;
 			ButtonsPressed++;
 			//friend += 2;
 			//sad--;
@@ -184,6 +225,11 @@ public class ButtonSelection1 : MonoBehaviour
 		}
 
 		if (Input.GetKeyDown (KeyCode.D) && StopButtons == false) { //Wink. Ups sad and love by 1
+			audioButton.pitch = audioButton.pitch + buttonPitch;
+			audioButton.clip = buttonClick;
+			audioButton.Play();
+			buttonPitch+=0.075f;
+			buttons [ButtonsPressed].GetComponent<SpriteRenderer> ().sprite = winkSprite;
 			ButtonsPressed++;
 			//sad++;
 			//love++;
@@ -200,9 +246,23 @@ public class ButtonSelection1 : MonoBehaviour
 			}
 		}
 
+		if (resetIcons == true) { //resets the button icons to blank
+			buttons [0].GetComponent<SpriteRenderer> ().sprite = blankSprite;
+			buttons [1].GetComponent<SpriteRenderer> ().sprite = blankSprite;
+			buttons [2].GetComponent<SpriteRenderer> ().sprite = blankSprite;
+			buttons [3].GetComponent<SpriteRenderer> ().sprite = blankSprite;
+			buttons [4].GetComponent<SpriteRenderer> ().sprite = blankSprite;
+
+			resetIcons = false;
+		}
+
 		if (ButtonsPressed >= MaxButtons) {
 			Timer = 0;
 			StopButtons = true;
+			if (playedEndAudio == false) {
+				roundEndAudio.Play ();
+			}
+			playedEndAudio = true;
 			friendChange = friendUp;
 			loveChange = loveUp;
 			sadChange = sadUp;
@@ -214,17 +274,13 @@ public class ButtonSelection1 : MonoBehaviour
 
 		}
 		if (TimeToChange < 0) {
-			
+
 			ButtonsPressed = 0;
-			if (RoundNumber < maxRound) {
-				RoundNumber++;
-			}else{
-				gameMaster.sadTotalGM = sadnessBar.value;
-				gameMaster.loveTotalGM = lovelyBar.value;
-				gameMaster.friendTotalGM = friendlyBar.value;
-				Application.LoadLevel ("Loren/scenes/endScene");
-				//button.SetActive(true);
-			}
+			playedEndAudio = false;
+			RoundNumber++;
+			resetIcons = true;
+			audioButton.pitch = baseButtonPitch;
+			buttonPitch = 0;
 			TimeToChange = 2f;
 			Timer = 11f;
 			StopButtons = false;
@@ -233,23 +289,23 @@ public class ButtonSelection1 : MonoBehaviour
 			roundButton.SetActive (true);
 
 		}else{
-				roundButton.SetActive(false);
-			}
+			roundButton.SetActive(false);
+		}
 
 	}
-//	void RoundChange(){
-//		if (RoundNumber < 4) {
-//			ButtonSelection.MaxButtons = 3;
-//			ButtonSelection.Timer = 16f;
-//		} else if (RoundNumber < 6) {
-//			ButtonSelection.MaxButtons = 4;
-//			ButtonSelection.Timer = 11f;
-//		} if (RoundNumber == 6) {
-//			ButtonSelection.MaxButtons = 5;
-//			ButtonSelection.Timer = 6f;
-//		}
-//	}
+	//	void RoundChange(){
+	//		if (RoundNumber < 4) {
+	//			ButtonSelection.MaxButtons = 3;
+	//			ButtonSelection.Timer = 16f;
+	//		} else if (RoundNumber < 6) {
+	//			ButtonSelection.MaxButtons = 4;
+	//			ButtonSelection.Timer = 11f;
+	//		} if (RoundNumber == 6) {
+	//			ButtonSelection.MaxButtons = 5;
+	//			ButtonSelection.Timer = 6f;
+	//		}
+	//	}
 	//void FixedUpdate (){
-		
+
 	//}
 }
---
+++ Assets/Loren/scripts/newInput.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class newInput : MonoBehaviour {
+	public string inputCombo;
+
+	public GameObject happyFace, sadFace, loveFace;
+	public GameObject flatter, joke, wink, chat, neutral;
+	public Text meterText;
+	public Text timeText;
+
+	public bool comboOver;
+	public bool StopButtons;
+	public string[] combos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Get player input. 
+		if (inputCombo.Length <= 5 && comboOver == false) {
+			if (Input.GetKeyDown (KeyCode.A)) {
+				inputCombo += "A";
+
+			} else if (Input.GetKeyDown (KeyCode.W)) {
+				Input += "W";
+			} else if (Input.GetKeyDown (KeyCode.S)) {
+				inputCombo += "A";
+
+			} else if (Input.GetKeyDown (KeyCode.D)) {
+				Input += "D";
+			}
+			else if(Input.GetKeyDown(KeyCode.Space)){
+				comboOver = true;	
+			}
+		}
+		else{ //COMPARE COMBOS
+
+
+
+
+		}
+	}
+	bool CompareCombo(string playerCombo, string[] comboList){
+		bool isIt = false;
+		//if(comboList.GetHashCode (playerCombo))
+		{
+			isIt = true;
+		}
+		return isIt;
+	}
+
+}
+
+
+
+
+
+
