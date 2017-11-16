1e8c5367162e9b02bc91f9dc38e763385b3580d7
Evan Schmidt <ejs552@nyu.edu>
2017-11-16 16:42:33 -0500

    Added test stuff

--- Assets/Scripts/comboFlavor.cs
+++ Assets/Scripts/comboFlavor.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class comboFlavor : MonoBehaviour {
 
+	public comboReader comboReader;
+	public comboManager comboManager;
+	int bigMood;
 	// Use this for initialization
 	void Start () {
-		
+		bigMood = (int)comboReader.responses [questionIndex].moodEffect;
 	}
 	
 	// Update is called once per frame
