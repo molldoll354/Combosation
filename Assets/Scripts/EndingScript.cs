ef71d45ffda1bec0035b3a9cd09c6b910da01eca
Molly <molly354@comcast.net>
2017-11-19 18:33:33 -0500

    added ending scene + base script for dialogue

---
+++ Assets/Scripts/EndingScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndingScript : MonoBehaviour {
+	public Text endText;
+	public TextAsset EndDialogue;
+	public string[] textLines;
+	int currentLine;
+	// Use this for initialization
+	void Start () {
+		if (EndDialogue != null) {
+			textLines = (EndDialogue.text.Split ('\n'));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		endText.text = EndDialogue [currentLine];
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			currentLine += 1;
+		}
+			
+
+	}
+}
