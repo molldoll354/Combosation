8755fe165d0c761957c9cc343d3a9fcedff74bb1
Molly <molly354@comcast.net>
2017-11-20 16:25:07 -0500

    ending scene (presumably) complete w/ anims and ranks

--- Assets/Scripts/EndingScript.cs
+++ Assets/Scripts/EndingScript.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EndingScript : MonoBehaviour {
 	public Text endText;
 	public TextAsset EndDialogue;
 	public string[] textLines;
-	int currentLine;
+	public int currentLine;
 
 	public Animator sakiFace;
 	public GameObject saki;
@@ -17,14 +18,6 @@ public class EndingScript : MonoBehaviour {
 			textLines = (EndDialogue.text.Split ('\n'));
 		}
 		sakiFace = saki.GetComponent<Animator> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		endText.text = textLines [currentLine];
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			currentLine += 1;
-		}
 
 		if (comboReader.statChecker <= -8) {
 			//F RANK
@@ -50,12 +43,60 @@ public class EndingScript : MonoBehaviour {
 			//S RANK
 			currentLine = 2;
 		}
-
-		if (currentLine == 2 || currentLine == 3) {
-			//sakiFace.Play(blush)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		endText.text = textLines [currentLine];
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			currentLine++;
 		}
-		if (currentLine == 4) {
-			//sakiFace.Play(heart)
+
+
+
+		if (currentLine == 2 || currentLine == 3 || currentLine == 9 || currentLine == 10) {
+			sakiFace.Play ("blush");
+		}
+		if (currentLine == 4 || currentLine == 11) {
+			sakiFace.Play ("love");
+		}
+		if (currentLine == 5 || currentLine == 12 || currentLine == 18 || currentLine == 20) {
+			sakiFace.Play ("happy");
+		}
+		if (currentLine == 16 || currentLine == 17 || currentLine == 26 || currentLine == 27) {
+			sakiFace.Play ("neutral");
+		}
+		if(currentLine == 24 || currentLine == 25 || currentLine == 31 || currentLine == 32){
+			sakiFace.Play ("mehREACT");
+		}
+		if (currentLine == 33 || currentLine == 34 || currentLine == 38 || currentLine == 39 || currentLine == 40) {
+			sakiFace.Play ("unimpressed");
+		}
+		if (currentLine == 41) {
+			sakiFace.Play ("angry");
+		}
+
+		if (currentLine == 6) {
+			sakiFace.Play ("Srank");
+		}
+		if (currentLine == 13) {
+			sakiFace.Play ("Arank");
+		}
+		if (currentLine == 21) {
+			sakiFace.Play ("Brank");
+		}
+		if (currentLine == 28) {
+			sakiFace.Play ("Crank");
+		}
+		if (currentLine == 35) {
+			sakiFace.Play ("Drank");
+		}
+		if (currentLine == 42) {
+			sakiFace.Play ("Frank");
+		}
+
+		if (currentLine == 7 || currentLine == 14 || currentLine == 22 || currentLine == 29 || currentLine == 36 || currentLine == 43) {
+			SceneManager.LoadScene ("dellapisoundscene");
 		}
 	}
 }
