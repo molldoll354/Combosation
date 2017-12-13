376f159e0a4cfb4daf27417ffed98162a572d5a7
Evan Schmidt <ejs552@nyu.edu>
2017-12-13 16:20:15 -0500

    Created title screen/worked with transitions

---
+++ Assets/Scripts/titleScreenManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titleScreenManager : MonoBehaviour {
+
+	float sceneEndTimer = 1f;
+	public Animator sceneEndAnim;
+	public GameObject sceneEndAnimObject;
+
+	public AudioSource transitionSource;
+
+
+	bool sceneEnding;
+	// Use this for initialization
+	void Start () {
+		sceneEndAnimObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (sceneEndTimer);
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			sceneEnding = true;
+			transitionSource.Play ();
+
+		}
+
+		if (sceneEnding == true) {
+			sceneEndTimer -= Time.deltaTime;
+			sceneEndAnimObject.SetActive (true);
+			sceneEndAnim.Play ("screenTransitionOpenAnimation");
+			if (sceneEndTimer <= 0) {
+				SceneManager.LoadScene ("tutorialScene");
+			}
+		}
+	}
+}
