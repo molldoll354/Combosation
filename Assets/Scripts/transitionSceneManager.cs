09438d85e5209e86853fbfb69db1dd97a1b803ad
Evan Schmidt <ejs552@nyu.edu>
2017-12-13 13:58:08 -0500

    Added tentative scene transition anims

--- Assets/Scripts/transitionSceneManager.cs
+++ Assets/Scripts/transitionSceneManager.cs
@@ -5,15 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class transitionSceneManager : MonoBehaviour {
 
+	float sceneEndTimer = 1f;
+	public Animator sceneEndAnim;
+	public GameObject sceneEndAnimObject;
+
+	public AudioSource transitionSource;
+	public AudioSource music;
+
+	bool sceneEnding;
 	// Use this for initialization
 	void Start () {
-		
+		sceneEndAnimObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.Log (sceneEndTimer);
 		if (Input.GetKeyDown(KeyCode.Space)) {
-			SceneManager.LoadScene ("MasterScene");
+			sceneEnding = true;
+			transitionSource.Play ();
+			music.Stop ();
+		}
+
+		if (sceneEnding == true) {
+			sceneEndTimer -= Time.deltaTime;
+			sceneEndAnimObject.SetActive (true);
+			sceneEndAnim.Play ("screenTransitionOpenAnimation");
+			if (sceneEndTimer <= 0) {
+				SceneManager.LoadScene ("MasterScene");
+			}
 		}
 	}
 }
