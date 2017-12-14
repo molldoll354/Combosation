4b9e3ae0148ae3d2ffd8be3ce173d0498fe690a8
Evan Schmidt <ejs552@nyu.edu>
2017-12-14 01:46:25 -0500

    Added end scene

---
+++ Assets/Scripts/endSceneFinal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endSceneFinal : MonoBehaviour {
+
+	public GameObject grade;
+	public GameObject medal;
+
+	public Sprite S;
+	public Sprite A;
+	public Sprite B;
+	public Sprite C;
+	public Sprite D;
+	public Sprite F;
+	public Sprite chatMedal;
+	public Sprite flatterMedal;
+	public Sprite flirtMedal;
+	public Sprite jokeMedal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
